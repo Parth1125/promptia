02575b280d0e5840b79008edba3718b165af27c9
Parth1125 <pgera326@gmail.com>
2024-03-12 09:30:28 +0530

    feat: added get prompt

--- components/Feed.jsx
+++ components/Feed.jsx
@@ -1,9 +1,52 @@
-import React from 'react'
+"use client";
+import { useState, useEffect } from "react";
+import PromptCard from "./PromptCard";
+import { SearchParamsContext } from "next/dist/shared/lib/hooks-client-context.shared-runtime";
 
-const Feed = () => {
+const PromptCardList = ({ data, handleTagClick }) => {
   return (
-    <div>Feed</div>
-  )
-}
+    <div className="mt-16 prompt_layout">
+      {data.map((post) => (
+        <PromptCard
+          key={post._id}
+          post={post}
+          handleTagClick={handleTagClick}
+        />
+      ))}
+    </div>
+  );
+};
+const Feed = () => {
+  const [searchText, setsearchText] = useState("");
+  const [posts, setposts] = useState([]);
+  const handleSearchChange = (e) => {
+    // setsearchText(e.target.value);
+  };
 
-export default Feed
+  useEffect(() => {
+    const fetchPosts = async () => {
+      const response = await fetch("/api/prompt");
+      const data = await response.json();
+      setposts(data);
+    };
+    fetchPosts();
+  }, []);
+  return (
+    <section className="feed">
+      <form className="relative w-full flex-center">
+        <input
+          type="text"
+          placeholder="Search for a Prompt"
+          value={searchText}
+          onChange={handleSearchChange}
+          required
+          className="search_input peer"
+        />
+      </form>
+
+      <PromptCardList data={posts} handleTagClick={() => {}} />
+    </section>
+  );
+};
+
+export default Feed;
