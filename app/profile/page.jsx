625a593d5742e01a72b422469c51627d28dda8d7
Parth1125 <pgera326@gmail.com>
2024-03-13 09:21:39 +0530

    added my profile page

--- app/profile/page.jsx
+++ app/profile/page.jsx
@@ -1,7 +1,35 @@
-import React from "react";
+"use client";
+import { useState, useEffect } from "react";
+import { useSession } from "next-auth/react";
+import { useRouter } from "next/router";
 
-const Profile = () => {
-  return <div>Profile</div>;
+import Profile from "@components/Profile";
+
+const MyProfile = () => {
+  const { data: session } = useSession();
+
+  const [Posts, setPosts] = useState([]);
+
+  useEffect(() => {
+    const fetchPosts = async () => {
+      const response = await fetch(`/api/users/${session?.user.id}/posts`);
+      const data = await response.json();
+      setPosts(data);
+    };
+    if (session?.user.id) fetchPosts();
+  }, []);
+  console.log(Posts);
+  const handleEdit = () => {};
+  const handleDelete = async () => {};
+  return (
+    <Profile
+      name="My"
+      desc="Welcome to your personalized profile page"
+      data={Posts}
+      handleEdit={handleEdit}
+      handleDelete={handleDelete}
+    />
+  );
 };
 
-export default Profile;
+export default MyProfile;
