3443c5cb91421efdd272141418d55d6e4928e50c
Parth1125 <pgera326@gmail.com>
2024-03-31 17:42:30 +0530

    changes in config

--- next.config.mjs
+++ next.config.mjs
@@ -7,7 +7,6 @@ const nextConfig = {
     config.experiments = {
       ...config.experiments,
       topLevelAwait: true,
-      missingSuspenseWithCSRBailout: false,
     };
     return config;
   },
